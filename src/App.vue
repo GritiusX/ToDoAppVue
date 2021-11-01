9ae1fc70dc68eaedd35471231d846a8dd1752bb6
GritiusX <guidopawluk@gmail.com>
2021-11-01 16:11:00 -0300

    commit de Vue To Do List

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
+<div class="container">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center mt-5">To Do List Vue!</h1>
+    <main class="mainContainer">
+        <input type="text" class="form-control w-50 me-3" @keyup.enter="add()" v-model="newToDo">
+            <button class="btn btn-primary" @click="add()">Add</button>
+    </main>
+	<div class="list">
+    <ul style="list-style: none" class="list">
+      <li class="listItem mt-3" v-for="(todo,i) in existingToDo" :key="todo.id">
+        <h3>
+          {{i+1}}: {{todo.text}}
+        </h3>
+        <button class="btn btn-danger ms-3" @click="deleteToDo(i)">X</button>
+      </li>
+    </ul>
+	</div>
+
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ data(){
+   return{
+     newToDo:'',
+     existingToDo:[
+       {text: 'To Do List Vue',id: 0},
+       {text: 'Do Groceries',id: 1},
+       {text: 'Walk dog',id: 2},
+     ],
+     
+   }
+ },
+ methods:{
+       add(){
+         this.existingToDo.push({
+           text:this.newToDo,
+           id: new Date().valueOf()
+         }),
+         this.newToDo = ''
+       },
+       deleteToDo(i){
+         this.existingToDo.splice(i,1)
+       }
+     }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+height:50%;
+}
+.container{
+	background: #41B883;
+	border-radius: 10px;
+	padding:10px;
+	margin-top: 50px;
+}
+.mainContainer{
+  display: flex;
+  flex-wrap:nowrap;
+  justify-content: center;
+}
+
+.list{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.listItem{
+	display: flex;
 }
 </style>
